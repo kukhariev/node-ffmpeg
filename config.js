694f9b474eab83edbe188e691d9fe73cc97e456f
kukhariev <q_h@inbox.ru>
2022-09-24 10:09:56 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+const ffmpegStaticPath = require('ffmpeg-static');
+const { spawnSync } = require('child_process');
+const { existsSync } = require('fs');
+const { tmpdir } = require('os');
+
+const tmp = tmpdir();
+const inputFile = `${__dirname}/test/fixtures/input.mp4`;
+const outputFile = `${tmp}/output.mp4`;
+const durationSec = 10.0;
+
+const ffmpegPath = (process.env.FFMPEG_PATH = String(process.env.FFMPEG_PATH || ffmpegStaticPath));
+
+function generateTestFile(filename, durationSec = 10.0) {
+  spawnSync(ffmpegPath, ['-y', '-f', 'lavfi', '-i', `testsrc=duration=${durationSec}:size=640x360:rate=30`, filename]);
+}
+
+if (!existsSync(inputFile)) {
+  generateTestFile(inputFile, durationSec);
+}
+
+exports.generateTestFile = generateTestFile;
+exports.inputFile = inputFile;
+exports.durationSec = durationSec;
+exports.outputFile = outputFile;
+exports.ffmpegPath = ffmpegPath;
---
+++ src/ffmpeg.ts
@@ -0,0 +1,110 @@
+import { FfmpegProgress, FfmpegProgressEvent } from '@dropb/ffmpeg-progress';
+import { ChildProcess, spawn } from 'child_process';
+import { noop, parse } from './utils';
+import { setPriority } from 'os';
+
+export type ExitEvent = {
+  code: number | null;
+  message?: string | null;
+  signal?: string | number | null;
+};
+
+export interface Options {
+  args?: string[] | string;
+  onProgress?: (evt: FfmpegProgressEvent) => void;
+  gracefullyExit?: boolean;
+  priority?: number;
+  timeout?: number;
+  // /**
+  //  * @internal
+  //  * Require nodejs >=16
+  //  */
+  // signal?: AbortSignal;
+}
+
+const controlSignals = ['SIGSTOP', 'SIGCONT', 18, 19];
+
+/**
+ * @todo Write the documentation.
+ */
+export class Ffmpeg {
+  process?: ChildProcess;
+  readonly killSignal?: NodeJS.Signals | number;
+  private onProgressCallback: (evt: FfmpegProgressEvent) => void = noop;
+  private _args: string[] = [];
+
+  constructor(public options: Options = {}) {
+    this.onProgressCallback = options.onProgress || noop;
+    if (options.args) {
+      this._args = typeof options.args === 'string' ? parse(options.args) : options.args;
+    }
+  }
+
+  onProgress(cb: (evt: FfmpegProgressEvent) => void): Ffmpeg {
+    this.onProgressCallback = cb;
+    return this;
+  }
+
+  setPriority(priority = 0): Ffmpeg {
+    if (this.process?.pid) {
+      setPriority(this.process.pid, priority);
+    } else {
+      this.options.priority = priority;
+    }
+    return this;
+  }
+
+  kill(signal: NodeJS.Signals = 'SIGKILL'): Ffmpeg {
+    if (this.process?.pid) {
+      this.process.kill(signal);
+      if (!controlSignals.includes(signal)) {
+        (this.killSignal as any) = signal;
+      }
+      return this;
+    }
+    throw new Error('No process running');
+  }
+
+  cancel(signal?: NodeJS.Signals): Ffmpeg {
+    return this.kill(signal);
+  }
+
+  run(args?: string[] | string): Promise<ExitEvent> {
+    return new Promise((resolve, reject) => {
+      const progressPipe = new FfmpegProgress();
+      if (args) {
+        this._args = typeof args === 'string' ? parse(args) : args;
+      }
+
+      this.process = spawn(process.env.FFMPEG_PATH || process.env.FFMPEG_BIN || 'ffmpeg', this._args, {
+        windowsHide: true,
+        timeout: this.options.timeout || 0,
+        // signal: this.options.signal, // nodejs >=16
+      });
+
+      this.process.on('error', reject);
+
+      this.process.on('close', (code, signal) => {
+        if (code === 0) {
+          resolve({ code, signal, message: 'ok' });
+        } else if (this.options.gracefullyExit && this.killSignal) {
+          resolve({ code, signal: signal || this.killSignal, message: progressPipe.exitMessage });
+        } else {
+          reject(new Error(progressPipe.exitMessage || 'Unknown Error'));
+        }
+      });
+
+      this.process.stderr?.pipe(progressPipe).on('data', (progressData: FfmpegProgressEvent) => {
+        this.onProgressCallback(progressData);
+      });
+
+      if (this.options.priority !== undefined) {
+        this.setPriority(this.options.priority);
+      }
+    });
+  }
+}
+
+export function createFfmpeg(options: Options = {}): Ffmpeg {
+  return new Ffmpeg(options);
+}
